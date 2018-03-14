6715106d705175c7df3bc6bb5a4d4f125c5c553b
Iris Wong <iwwong@sfu.ca>
2018-03-14 16:48:48 -0700

    Created a mobile menu

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -10,10 +10,16 @@
 //   }
 // });
 
-function toggle_visibility(id) {
-  var e = document.getElementById(id);
-  if(e.style.display == 'block')
-  e.style.display = 'none';
-  else
-  e.style.display = 'block';
-}
+// function toggle_visibility(id) {
+//   var e = document.getElementById(id);
+//   if(e.style.display == 'block')
+//   e.style.display = 'none';
+//   else
+//   e.style.display = 'block';
+// }
+
+// Credit: Mobile Menu Toggle: https://stackoverflow.com/questions/32799858/when-click-on-menu-it-opens-when-click-again-it-close
+
+$('button').click(function() {
+  $('.mobile-menu-items').slideToggle();
+});
